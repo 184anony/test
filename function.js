d2c7bfe1782407242b014016e09d15a6d7718ef9
184anony <49863944+184anony@users.noreply.github.com>
2023-04-20 15:25:39 +0900

    Update function.js

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ let result;
 let str = "これはテスト用の文章です。"
 	
 kuromoji.builder({dicPath: DICT_PATH}).build((err, tokenizer)=>{
-	const tokens = tokenizer.tokenize(str);// 解析データの取得
+	tokens = tokenizer.tokenize(str);// 解析データの取得
 	tokens.forEach((token)=>{// 解析結果を順番に取得する
 		console.log(token);
 	});
